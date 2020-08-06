ebd2acdfb7e114de947a66c7707424e03a0e1496
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-08-06 17:01:28 +0200

    -setting up utils -setting up startup

--- PunishmentManager-spigot_side/PunishmentManager/src/main/java/me/superbiebel/punishmentmanager/PunishmentManager.java
+++ PunishmentManager-spigot_side/PunishmentManager/src/main/java/me/superbiebel/punishmentmanager/PunishmentManager.java
@@ -1,17 +1,34 @@
 package me.superbiebel.punishmentmanager;
 
+import me.superbiebel.punishmentmanager.Utils.Log;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PunishmentManager extends JavaPlugin {
 
+    private static boolean debugMode;
+    private static FileConfiguration config;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        loadConfig();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        Log.debug("Thank you for using PunishmentManager! Bye!");
     }
+
+    public void loadConfig() {
+        this.saveDefaultConfig();
+        this.config= this.getConfig();
+        this.config.getBoolean("debug");
+    }
+    public static boolean getDebugMode() {
+        return debugMode;
+    }
+    public static FileConfiguration giveConfig() {
+        return config;
+    }
+
 }
