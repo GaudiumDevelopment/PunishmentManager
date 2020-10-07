d00dd2f35b106d8a697a57d4063b6dc794f6d9c0
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-10-07 18:17:41 +0200

    fixed initialized in Cache to be always false
    Signed-off-by: superbiebel <63414167+superbiebel@users.noreply.github.com>

--- PunishmentManager-spigot_side/PunishmentManager/src/main/java/me/superbiebel/punishmentmanager/data/Cache.java
+++ PunishmentManager-spigot_side/PunishmentManager/src/main/java/me/superbiebel/punishmentmanager/data/Cache.java
@@ -3,7 +3,6 @@ package me.superbiebel.punishmentmanager.data;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import me.lucko.helper.Schedulers;
-import me.superbiebel.punishmentmanager.PunishmentManager;
 import me.superbiebel.punishmentmanager.utils.Log;
 
 public class Cache {
@@ -14,8 +13,9 @@ public class Cache {
     public static void initCache(String db) {
         if (!isInitialized) {
             Schedulers.async().run(()-> {
+                isInitialized = true;
                 cacheDataSourceConfig = new HikariConfig();
-                //stole some code from the AdvancedBan plugin from DevLeoko
+                //stole some code from the AdvancedBan plugin from DevLeoko. this is actually his code but adapted to me.
                 String driverClassName = "org.hsqldb.jdbc.JDBCDriver";
                 try {
                     Class.forName(driverClassName);
@@ -23,12 +23,17 @@ public class Cache {
                     e.printStackTrace();
                 }
                 cacheDataSourceConfig.setDriverClassName(driverClassName);
-                cacheDataSourceConfig.setJdbcUrl("jdbc:hsqldb:mem:file:" + PunishmentManager.getPlugin().getDataFolder().getPath() + "/data/storage;hsqldb.lock_file=false");
+                cacheDataSourceConfig.setJdbcUrl("jdbc:hsqldb:mem:hsqldb.lock_file=false");
                 cacheDataSourceConfig.setUsername("PunishmentManager");
                 cacheDataSourceConfig.setPassword("");
                 Log.info("Don't worry about the error about a driver not supported, this is alright. Do NOT report this to my github!");
                 cacheDataSource = new HikariDataSource(cacheDataSourceConfig);
             })
-        ;}
+        ;} else {
+            Log.warning("Tried to initialize the cache, but it was already initialized, pls report to developer.");
+        }
+    }
+    public static HikariDataSource getCacheDataSource() {
+        return cacheDataSource;
     }
 }
