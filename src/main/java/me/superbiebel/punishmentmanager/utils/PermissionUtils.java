68008a6322a9733e5d91c47399d190ee078db398
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-10-24 10:15:14 +0200

    added check without message in PermissionUtils added @NotNull annotations for the args

--- src/main/java/me/superbiebel/punishmentmanager/utils/PermissionUtils.java
+++ src/main/java/me/superbiebel/punishmentmanager/utils/PermissionUtils.java
@@ -2,13 +2,19 @@ package me.superbiebel.punishmentmanager.utils;
 
 import me.superbiebel.punishmentmanager.PunishmentManager;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
 
 public class PermissionUtils {
-    public static boolean check(Player p, String permission) {
+    public static boolean check(@NotNull Player p, @NotNull String permission) {
+        return p.hasPermission(permission);
+    }
+
+    public static boolean checkAndMessage(@NotNull Player p, @NotNull String permission) {
         if (!p.hasPermission(permission)) {
-            ColorUtils.colorize(PunishmentManager.giveConfig().getString("messages.noPermissionMessage"));
+            p.sendMessage(ColorUtils.colorize(PunishmentManager.giveConfig().getString("messages.noPermissionMessage")));
             return false;
+        } else {
+            return true;
         }
-        return true;
     }
 }
