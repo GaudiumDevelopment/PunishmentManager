18e75b0f2ce9434447e33db79a35687deaa20fef
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-10-25 10:27:11 +0100

    made the file uploading to bintray faster and in 1 curl command

--- scripts/upload.bash
+++ scripts/upload.bash
@@ -1,5 +1,3 @@
 #!/bin/bash
 mv /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-NOT-REAL-VERSION.jar /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-INDEV_build_$1.jar
-curl -T /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-INDEV_build_$1.jar -usuperbiebel:$2 https://api.bintray.com/content/moderationmanager/PunishmentManager/INDEV/INDEV_build_$1/
-sleep 10
-curl -usuperbiebel:$2 -X POST https://api.bintray.com/content/moderationmanager/PunishmentManager/INDEV/INDEV_build_$1/publish
+curl -T /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-INDEV_build_$1.jar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/INDEV/INDEV_build_$1/PunishmentManager-INDEV_build_$1.jar\?publish=1
