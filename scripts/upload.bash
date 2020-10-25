270fc7d057925290a412cea63fc18f0fa3d97f9e
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-10-25 15:14:54 +0100

    Revert "indev sync with dev"

---
+++ scripts/upload.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+mv /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-NOT-REAL-VERSION.jar /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-INDEV_build_$1.jar
+curl -T /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-INDEV_build_$1.jar -usuperbiebel:$2 https://api.bintray.com/content/moderationmanager/PunishmentManager/INDEV/INDEV_build_$1/
+sleep 10
+curl -usuperbiebel:$2 -X POST https://api.bintray.com/content/moderationmanager/PunishmentManager/INDEV/INDEV_build_$1/publish
