7ff473e5d2c57d1c60f3d42c5a28e6c385bfb02f
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-11-12 16:11:57 +0100

    removed id bucket

--- src/main/java/me/superbiebel/punishmentmanager/data/DataUtility.java
+++ src/main/java/me/superbiebel/punishmentmanager/data/DataUtility.java
@@ -1,8 +1,5 @@
 package me.superbiebel.punishmentmanager.data;
 
-import me.lucko.helper.bucket.Bucket;
-import me.lucko.helper.bucket.factory.BucketFactory;
-import me.lucko.helper.bucket.partitioning.PartitioningStrategies;
 import me.lucko.helper.metadata.MetadataKey;
 import org.bukkit.entity.Player;
 
@@ -12,7 +9,6 @@ public class DataUtility {
 
     public static final MetadataKey<Player> CRIMINAL_KEY = MetadataKey.create("criminal",Player.class);
     public static final MetadataKey<List> ALLOWED_IDS_KEY = MetadataKey.create("allowed_ids",List.class);
-    public static final Bucket<Player> id_bucket = BucketFactory.newHashSetBucket(20, PartitioningStrategies.nextInCycle());
     public static void init() {
 
     }
