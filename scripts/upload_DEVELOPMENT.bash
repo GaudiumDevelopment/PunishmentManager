bc78f19ab8bc8f5de47eeccca9a53de5d0bb97f2
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-12-08 12:47:07 +0100

    hopefully fix the build failures 4

--- scripts/upload_DEVELOPMENT.bash
+++ scripts/upload_DEVELOPMENT.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-mv /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-NOT-REAL-VERSION.jar /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-build_$1.jar
-curl -T /Users/travis/.m2/repository/me/superbiebel/PunishmentManager/NOT-REAL-VERSION/PunishmentManager-build_$1.jar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/INDEV/build_$1/PunishmentManager-build_$1.jar\?publish=1
+mv /Users/travis/.m2/repository/me/superbiebel/PunishmentManagerCore/NOT-REAL-VERSION/PunishmentManagerCore-NOT-REAL-VERSION.jar /Users/travis/.m2/repository/me/superbiebel/PunishmentManagerCore/NOT-REAL-VERSION/PunishmentManagerCore-build_$1.jar
+curl -T /Users/travis/.m2/repository/me/superbiebel/PunishmentManagerCore/NOT-REAL-VERSION/PunishmentManagerCore-build_$1.jar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/DEVELOPMENT/build_$1/PunishmentManagerCore-build_$1.jar\?publish=1
