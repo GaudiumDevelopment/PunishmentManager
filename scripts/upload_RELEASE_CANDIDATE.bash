949277b3cfb26888a848a7a5909c4fe907a2fadc
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-12-11 19:18:46 +0100

    added the "+default_module" in the tar file name

--- scripts/upload_RELEASE_CANDIDATE.bash
+++ scripts/upload_RELEASE_CANDIDATE.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir /Users/travis/PunishmentManager-build_$1
-mv "/Users/travis/.m2/repository/me/superbiebel/PunishmentManagerCore/NOT-REAL-VERSION/PunishmentManagerCore-NOT-REAL-VERSION.jar" "/Users/travis/PunishmentManager-build_$1/PunishmentManagerCore-build_$1.jar"
+mv "/Users/travis/.m2/repository/me/superbiebel/PunishmentManagerCore/NOT-REAL-VERSION/PunishmentManagerCore-NOT-REAL-VERSION.jar" "/Users/travis/PunishmentManagerCore-build_$1.jar"
 mv "/Users/travis/.m2/repository/me/superbiebel/defaultehcache-NOT-REAL-VERSION.jar" "/Users/travis/PunishmentManager-build_$1/defaultehcache-build_$1.jar"
 mv "/Users/travis/.m2/repository/me/superbiebel/defaultdatahandler-NOT-REAL-VERSION.jar" "/Users/travis/PunishmentManager-build_$1/defaultdatahandler-build_$1.jar"
 mv "/Users/travis/.m2/repository/me/superbiebel/defaultmysqldatabase-NOT-REAL-VERSION.jar" "/Users/travis/PunishmentManager-build_$1/defaultmysqldatabase-build_$1.jar"
@@ -9,6 +9,5 @@ cd /Users/travis/
 tar -cvzf PunishmentManager-build_$1.tar PunishmentManager-build_$1
 
 
-curl -T /Users/travis/PunishmentManager-build_$1.tar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/RELEASE_CANDIDATE/build_$1/PunishmentManager-build_$1.tar\?publish=1
-curl -T /Users/travis/PunishmentManager-build_$1 -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/RELEASE_CANDIDATE/build_$1/PunishmentManager-build_$1\?publish=1
+curl -T /Users/travis/PunishmentManager+default_modules-build_$1.tar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/RELEASE_CANDIDATE/build_$1/PunishmentManager+default_modules-build_$1.tar\?publish=1
 curl -T /Users/travis/PunishmentManager-build_$1/PunishmentManagerCore-build_$1.jar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/RELEASE_CANDIDATE/build_$1/PunishmentManager-build_$1.jar\?publish=1
