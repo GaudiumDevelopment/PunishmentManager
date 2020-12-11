32ec2bd9b3774bf43b79fd5f4dbed1ac48895f11
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-12-11 18:38:01 +0100

    added file compressing

--- scripts/upload_DEVELOPMENT.bash
+++ scripts/upload_DEVELOPMENT.bash
@@ -1,3 +1,14 @@
 #!/bin/bash
-mv /Users/travis/.m2/repository/me/superbiebel/PunishmentManagerCore/NOT-REAL-VERSION/PunishmentManagerCore-NOT-REAL-VERSION.jar /Users/travis/.m2/repository/me/superbiebel/PunishmentManagerCore/NOT-REAL-VERSION/PunishmentManagerCore-build_$1.jar
-curl -T /Users/travis/.m2/repository/me/superbiebel/PunishmentManagerCore/NOT-REAL-VERSION/PunishmentManagerCore-build_$1.jar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/DEVELOPMENT/build_$1/PunishmentManagerCore-build_$1.jar\?publish=1
+
+mkdir /Users/travis/PunishmentManager-build_$1
+mv "/Users/travis/.m2/repository/me/superbiebel/PunishmentManagerCore/NOT-REAL-VERSION/PunishmentManagerCore-NOT-REAL-VERSION.jar" "/Users/travis/PunishmentManager-build_$1/PunishmentManagerCore-build_$1.jar"
+mv "/Users/travis/.m2/repository/me/superbiebel/defaultehcache-NOT-REAL-VERSION.jar" "/Users/travis/PunishmentManager-build_$1/defaultehcache-build_$1.jar"
+mv "/Users/travis/.m2/repository/me/superbiebel/defaultdatahandler-NOT-REAL-VERSION.jar" "/Users/travis/PunishmentManager-build_$1/defaultdatahandler-build_$1.jar"
+mv "/Users/travis/.m2/repository/me/superbiebel/defaultmysqldatabase-NOT-REAL-VERSION.jar" "/Users/travis/PunishmentManager-build_$1/defaultmysqldatabase-build_$1.jar"
+cd /Users/travis/
+tar -cvzf PunishmentManager-build_$1.tar PunishmentManager-build_$1
+
+
+curl -T /Users/travis/PunishmentManager-build_$1.tar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/DEVELOPMENT/build_$1/PunishmentManager-build_$1.tar\?publish=1
+curl -T /Users/travis/PunishmentManager-build_$1 -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/DEVELOPMENT/build_$1/PunishmentManager-build_$1\?publish=1
+curl -T /Users/travis/PunishmentManager-build_$1/PunishmentManagerCore-build_$1.jar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/DEVELOPMENT/build_$1/PunishmentManager-build_$1.jar\?publish=1
