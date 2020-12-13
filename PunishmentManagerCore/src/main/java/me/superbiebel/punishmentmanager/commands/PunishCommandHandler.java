c825bc6fb539db731fde8c8e7422dcac082fc7e3
superbiebel <63414167+superbiebel@users.noreply.github.com>
2020-12-13 15:23:26 +0100

    removed hardcoded logtofile boolean in logexception

--- PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/commands/PunishCommandHandler.java
+++ PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/commands/PunishCommandHandler.java
@@ -4,6 +4,7 @@ import cloud.commandframework.annotations.Argument;
 import cloud.commandframework.annotations.CommandMethod;
 import org.bukkit.command.CommandSender;
 
+@Deprecated(forRemoval = true)
 public class PunishCommandHandler {
     @CommandMethod("betterpunish <testinput>")
     public void punishcommand(CommandSender sender, @Argument("testinput") String testinput){
