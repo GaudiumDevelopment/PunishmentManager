1d466e8d82af06aa438412c9fc375c1185b81094
Superbiebel <superbiebel@gmail.com>
2021-01-17 13:49:29 +0100

    added setter to HistoryRecord added constructor for HistoryRecord added some extra fields to allow ImprovedHistoryChestGui to use it

--- PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/data/dataObjects/HistoryRecord.java
+++ PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/data/dataObjects/HistoryRecord.java
@@ -1,23 +1,54 @@
 package me.superbiebel.punishmentmanager.data.dataObjects;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.bukkit.Material;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
-public class HistoryRecord {
+import java.util.UUID;
+
+public class HistoryRecord implements Comparable<HistoryRecord>{
+    @Setter
     @Getter
-    int uuid;
+    private long creationdate;
+    @Setter
     @Getter
-    Material material;
+    private UUID uuid;
+    @Setter
     @Getter
-    int offenseId;
+    private Material material;
+    @Setter
     @Getter
-    int punishmentId;
-    
+    private int offenseId;
+    @Setter
+    @Getter
+    private int punishmentId;
+    @Setter
+    @Getter
+    private String formattedReason;
+    @Setter
+    @Getter
+    private int jailid;
+    @Setter
+    @Getter
+    private String offenseName;
+
+    public HistoryRecord(UUID historyID, Material material, int offenseId, int punishmentId, String offenseName, String formattedReason, int jailid, long creationdate, long muteduration, long banduration) {
+        this.uuid = historyID;
+        this.material = material;
+        this.offenseId = offenseId;
+        this.punishmentId = punishmentId;
+        this.formattedReason = formattedReason;
+        this.jailid = jailid;
+        this.offenseName = offenseName;
+        this.creationdate = creationdate;
+    }
+
     public HistoryRecord() {
     }
-    public HistoryRecord(int uuid, String material, int offenseId, int punishmentId, String formattedReason, @Nullable int jailid) {
-    
+
+    @Override
+    public int compareTo(@NotNull HistoryRecord o) {
+        return (int) (this.creationdate - o.getCreationdate());
     }
-    
 }
