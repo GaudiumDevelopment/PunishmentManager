5b532acf946d713f8fd0242e6cf4f9824b1f53f3
Superbiebel <superbiebel@gmail.com>
2021-01-27 21:58:37 +0100

    hopefully fix build uploads

--- scripts/upload_RELEASE_CANDIDATE.bash
+++ scripts/upload_RELEASE_CANDIDATE.bash
@@ -23,4 +23,4 @@ echo "Files archived!"
 echo "Uploading Files!"
 curl -T /Users/travis/PunishmentManager+default_modules-build_$1.tar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/RELEASE_CANDIDATE/build_$1/PunishmentManager+default_modules-build_$1.tar\?publish=1
 curl -T /Users/travis/PunishmentManager-build_$1/PunishmentManagerCore-build_$1.jar -usuperbiebel:$2 -X PUT https://api.bintray.com/content/moderationmanager/PunishmentManager/RELEASE_CANDIDATE/build_$1/PunishmentManager-build_$1.jar\?publish=1
-echo "Files successfully uploaded!"
+echo "Files uploading done!!"
