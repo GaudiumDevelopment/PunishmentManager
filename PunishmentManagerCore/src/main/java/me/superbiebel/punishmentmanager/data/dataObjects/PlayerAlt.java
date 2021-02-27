134d3b831f7385934aa0564f7914a38bc873dbd2
Superbiebel <superbiebel@gmail.com>
2021-02-27 18:07:51 +0100

    created playerAlt data class

---
+++ PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/data/dataObjects/PlayerAlt.java
@@ -0,0 +1,12 @@
+package me.superbiebel.punishmentmanager.data.dataObjects;
+
+import java.util.UUID;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PlayerAlt {
+
+    private UUID playerUUID;
+}
