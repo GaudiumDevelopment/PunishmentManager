be9c22d6f5441f2965a81534ea1baa48209c43fc
Superbiebel <superbiebel@gmail.com>
2021-04-03 19:26:03 +0200

    removed unnecessary space in method args

--- PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/offenseprocessing/abstraction/OffenseProcessor.java
+++ PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/offenseprocessing/abstraction/OffenseProcessor.java
@@ -3,5 +3,5 @@ package me.superbiebel.punishmentmanager.offenseprocessing.abstraction;
 import java.util.UUID;
 
 public interface OffenseProcessor {
-    void execute(UUID uuid,  Object[] args);
+    void execute(UUID uuid, Object[] args);
 }
