7edd2fb32f781f68a9cecbf1f40dca87fff7317b
Superbiebel <superbiebel@gmail.com>
2021-04-03 19:26:50 +0200

    removed unnecessary public declaration

--- PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/offenseprocessing/abstraction/OffenseProcessorFactory.java
+++ PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/offenseprocessing/abstraction/OffenseProcessorFactory.java
@@ -1,7 +1,7 @@
 package me.superbiebel.punishmentmanager.offenseprocessing.abstraction;
 
 public interface OffenseProcessorFactory {
-    public void init() throws Exception;
-    public OffenseProcessor giveOffenseProcessor() throws Exception;
+    void init() throws Exception;
+    OffenseProcessor giveOffenseProcessor() throws Exception;
 
 }
