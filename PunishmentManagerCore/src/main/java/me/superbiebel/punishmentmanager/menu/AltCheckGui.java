7ff69361af6b2c7e6f9ed794b17ca18006c02077
Superbiebel <superbiebel@gmail.com>
2021-04-07 22:39:32 +0200

    added back button

--- PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/menu/AltCheckGui.java
+++ PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/menu/AltCheckGui.java
@@ -1,12 +1,23 @@
 package me.superbiebel.punishmentmanager.menu;
 
+import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import com.github.stefvanschie.inventoryframework.pane.Pane.Priority;
+import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import me.lucko.helper.metadata.Metadata;
 import me.superbiebel.punishmentmanager.data.DATAKEYS;
 import me.superbiebel.punishmentmanager.menu.abstraction.AbstractChestGui;
+import me.superbiebel.punishmentmanager.utils.ColorUtils;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 public class AltCheckGui extends AbstractChestGui {
+
+    ItemStack backButtonItemstack;
+    GuiItem backButtonItem;
+    StaticPane buttonPane;
+
     @Override
     public void open(Player p) {
         cachedPlayer = p;
@@ -23,5 +34,21 @@ public class AltCheckGui extends AbstractChestGui {
         super.gui = new ChestGui(6,"Alts of " + Metadata.provideForPlayer(player).get(DATAKEYS.CRIMINAL_KEY)
                 .orElseThrow(()-> new NullPointerException("Could not find the player in the map to alt check for!"))
                 .getName());
+
+
+        backButtonItemstack = new ItemStack(Material.BARRIER);
+        backButtonItem = new GuiItem(backButtonItemstack);
+        backButtonItem.setAction(event->{
+            event.setCancelled(true);
+            ActionListGui actionListGui = new ActionListGui();
+            actionListGui.construct(false, false,(Player) event.getWhoClicked());
+            actionListGui.open();
+        });
+        backButtonItemstack.getItemMeta().setDisplayName(ColorUtils.colorize("&c&lBack"));
+
+
+        buttonPane = new StaticPane(0,5,9,1, Priority.HIGHEST);
+        buttonPane.addItem(backButtonItem,4,0);
+        super.gui.addPane(buttonPane);
     }
 }
