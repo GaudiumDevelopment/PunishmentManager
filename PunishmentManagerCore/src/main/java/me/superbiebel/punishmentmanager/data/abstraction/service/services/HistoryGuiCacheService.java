26ad02afd58717614eb3e4c1d43e0e7cc139610d
Superbiebel <superbiebel@gmail.com>
2021-04-09 21:18:27 +0200

    removed gui caching (complex stuff not needed anymore)

--- PunishmentManagerCore/src/main/java/me/superbiebel/punishmentmanager/data/abstraction/service/services/HistoryGuiCacheService.java
+++
@@ -1,6 +0,0 @@
-package me.superbiebel.punishmentmanager.data.abstraction.service.services;
-
-import me.superbiebel.punishmentmanager.data.abstraction.service.Service;
-
-public interface HistoryGuiCacheService extends Service {
-}
